3f502748cdbe6ff7a730c59026a48f1d9ecef479
RedFox199 <montherrt@gmail.com>
2026-04-08 10:27:37 +0200

    Update back.py

--- socialmedia/back.py
+++ socialmedia/back.py
@@ -300,4 +300,5 @@ def deletenotif(notification_id):
      
 
 if __name__== "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
