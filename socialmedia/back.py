15e5e6d41e1c7e613cb5b0355f9547b105e54a93
RedFox199 <montherrt@gmail.com>
2026-04-21 12:46:27 +0200

    Update back.py

--- socialmedia/back.py
+++ socialmedia/back.py
@@ -10,6 +10,7 @@ import requests
 from google.oauth2 import id_token
 from google.auth.transport import requests as grequests
 import shutil
+import json
 
 from flask import Flask, request,jsonify, render_template, redirect, url_for, session
 from pymongo import MongoClient
