19b4e7b1edc0fcea5ef1c9096ed74d6239f3f078
RedFox199 <montherrt@gmail.com>
2026-04-21 12:25:54 +0200

    Update back.py

--- socialmedia/back.py
+++ socialmedia/back.py
@@ -1,7 +1,15 @@
 from datetime import datetime
+import email
+import pathlib
+import uuid
 from xmlrpc import client
 from bson import ObjectId
 from dotenv import load_dotenv
+from google_auth_oauthlib.flow import Flow
+import requests
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
+import shutil
 
 from flask import Flask, request,jsonify, render_template, redirect, url_for, session
 from pymongo import MongoClient
@@ -20,13 +28,59 @@ uri = os.getenv("MONGO_URI")
 client = MongoClient(uri)
 db = client['Void_media']
 
+GOOGLE_CLIENT_ID = "78105620575-9hmje8ja5vtn4bamf6gjkfqmljhbb0q2.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file , scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"], redirect_uri="http://127.0.0.1:5000/callback")
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
 def allowed_file(filename):
      return "." in filename and filename.rsplit(".",1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
 def home():
+     session.clear()
      return render_template("sign-log.html")
+
+@app.route("/google-login")
+def google_login():
+     authorization_url, state = flow.authorization_url()
+     session["state"] = state
+     return redirect(authorization_url)
+
+@app.route("/callback")
+def callback():
+     state = session["state"]
+     flow.fetch_token(authorization_response=request.url)
+     if not session["state"] == request.args.get("state"):
+          return jsonify({"error": "state mismatch"}), 400
+     credentials = flow.credentials
+     request_adapter = grequests.Request()
+     id_info = id_token.verify_oauth2_token(
+     credentials._id_token,
+     request_adapter,
+     GOOGLE_CLIENT_ID
+     )
+     email = id_info.get("email")
+     name = id_info.get("name")
+     picture = "default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.webp" 
+
+    # ✅ Check if user exists
+     user = db.users.find_one({"email": email})
+
+     if not user:
+        new_user = db.users.insert_one({
+            "name": name,
+            "email": email,
+            "password": None,
+            "imgpath": picture,
+            "description": ""
+        })
+        session["user_id"] = str(new_user.inserted_id)
+     else:
+        session["user_id"] = str(user["_id"])
+
+     return redirect(url_for("showposts"))
 
 @app.route("/signup")
 def signup():
