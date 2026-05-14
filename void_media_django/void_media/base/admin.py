2fc8e3dce8f822d3f6e9d7f425ed35e975257b39
RedFox199 <montherrt@gmail.com>
2026-05-14 18:18:41 +0200

    Add files via upload

---
+++ void_media_django/void_media/base/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Posts, comments, likes, friends, friend_requests, messages, notifications
+
+# Register your models here.
+admin.site.register(Posts)
+admin.site.register(comments)
+admin.site.register(likes)
+admin.site.register(friends)
+admin.site.register(friend_requests)
+admin.site.register(messages)
+admin.site.register(notifications)
+
+
+
