d5acf74bf0347660347f4b93099723faf2301094
RedFox199 <montherrt@gmail.com>
2026-05-14 20:02:38 +0200

    Update settings.py

--- void_media_django/void_media/settings.py
+++ void_media_django/void_media/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'void_media.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'os.environ.get('DB_NAME')',
-        'USER': 'os.environ.get('DB_USER')',
-        'PASSWORD': 'os.environ.get('DB_PASSWORD')',
-        'HOST': 'os.environ.get('DB_HOST')',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
     }
 }
